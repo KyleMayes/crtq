3f663c79f119cb4acf2cc1dc427e0c3519f2293d
Kyle Mayes <kyle@mayeses.com>
2017-04-14 18:46:02 -0400

    Add Cargo project files

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+// Copyright 2017 Kyle Mayes
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! A multi-producer, multi-consumer wait-free queue.
+//!
+//! * [A Wait-Free Queue with Wait-Free Memory Reclamation](https://github.com/pramalhe/ConcurrencyFreaks/blob/master/papers/crturnqueue-2016.pdf)
+
+#![warn(missing_copy_implementations, missing_debug_implementations, missing_docs)]
+
+#![cfg_attr(feature="clippy", feature(plugin))]
+#![cfg_attr(feature="clippy", plugin(clippy))]
+#![cfg_attr(feature="clippy", warn(clippy))]
